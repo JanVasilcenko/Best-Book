5e1130cf6822185a0d56470f22f39fa134982189
Jan Vasilcenko <jan.vasilcenko@gnj.cz>
2021-04-19 15:46:15 +0200

    Log in, register, verification and forgotten password implemented with firebase

--- app/src/main/java/com/example/bestbook/ui/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/example/bestbook/ui/splashscreen/SplashScreenActivity.java
@@ -1,4 +1,4 @@
-package com.example.bestbook;
+package com.example.bestbook.ui.splashscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,6 +6,9 @@ import android.os.Handler;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bestbook.R;
+import com.example.bestbook.ui.login.LoginScreenActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
     private Handler handler = new Handler();
